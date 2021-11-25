5fef42791367f0dd50b75bf04e3d8ed37b5f904b
ksertas <k.serkanertas@gmail.com>
2021-11-25 20:55:21 +0100

    added group, media, actor repositories

--- src/main/java/nl/serkanertas/filmspringserver/repository/FilmRepository.java
+++ src/main/java/nl/serkanertas/filmspringserver/repository/FilmRepository.java
@@ -1,8 +1,8 @@
 package nl.serkanertas.filmspringserver.repository;
 
-import nl.serkanertas.filmspringserver.model.User;
+import nl.serkanertas.filmspringserver.model.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface FilmRepository extends JpaRepository<Film, Long> {}

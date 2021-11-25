3cb31e03dc2d8fcf310fff93c6363ace54f6dfc0
ksertas <k.serkanertas@gmail.com>
2021-11-25 20:39:47 +0100

    added group model

---
+++ src/main/java/nl/serkanertas/filmspringserver/model/Group.java
@@ -0,0 +1,21 @@
+package nl.serkanertas.filmspringserver.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "groups")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long group_id;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_group",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> usersInGroup = new ArrayList<>();
+}

9b66c7546771bd1142e927f48dec40e688956f9b
ksertas <k.serkanertas@gmail.com>
2021-11-26 16:59:45 +0100

    disable security login

--- src/main/java/nl/serkanertas/filmspringserver/FilmspringServerApplication.java
+++ src/main/java/nl/serkanertas/filmspringserver/FilmspringServerApplication.java
@@ -2,8 +2,9 @@ package nl.serkanertas.filmspringserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class FilmspringServerApplication {
 
 	public static void main(String[] args) {

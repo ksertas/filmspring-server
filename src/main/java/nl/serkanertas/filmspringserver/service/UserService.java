821803c5ade88b9e35389c914bcc41a3702022b4
ksertas <k.serkanertas@gmail.com>
2021-11-26 16:58:15 +0100

    add user creation in service

--- src/main/java/nl/serkanertas/filmspringserver/service/UserService.java
+++ src/main/java/nl/serkanertas/filmspringserver/service/UserService.java
@@ -1,6 +1,8 @@
 package nl.serkanertas.filmspringserver.service;
 
+import nl.serkanertas.filmspringserver.model.Film;
 import nl.serkanertas.filmspringserver.model.User;
+import nl.serkanertas.filmspringserver.repository.FilmRepository;
 import nl.serkanertas.filmspringserver.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,16 @@ public class UserService  {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private FilmRepository filmRepository;
+
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
 
 }

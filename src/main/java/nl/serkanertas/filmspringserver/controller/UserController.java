584db2d67deb8f52a4d5dd064979a96430f3e9fb
ksertas <k.serkanertas@gmail.com>
2021-11-26 16:57:48 +0100

    add user creation in controller

--- src/main/java/nl/serkanertas/filmspringserver/controller/UserController.java
+++ src/main/java/nl/serkanertas/filmspringserver/controller/UserController.java
@@ -1,15 +1,17 @@
 package nl.serkanertas.filmspringserver.controller;
 
+import nl.serkanertas.filmspringserver.model.Film;
 import nl.serkanertas.filmspringserver.model.User;
 import nl.serkanertas.filmspringserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
@@ -19,4 +21,11 @@ public class UserController {
     ResponseEntity<Object> getAllUsers() {
         return ResponseEntity.ok().body(userService.getAllUsers());
     }
+
+    @PostMapping("/create")
+    ResponseEntity<Object> createUser(@RequestBody User user) {
+        userService.createUser(user);
+        return new ResponseEntity<Object>(HttpStatus.CREATED);
+    }
+
 }

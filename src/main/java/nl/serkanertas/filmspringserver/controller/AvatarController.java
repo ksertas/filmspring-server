9dae4533c16ea18705be410b0f50fd57b24cac3a
ksertas <k.serkanertas@gmail.com>
2021-12-05 20:26:17 +0100

    add separation of user and group avatars

--- src/main/java/nl/serkanertas/filmspringserver/controller/AvatarController.java
+++ src/main/java/nl/serkanertas/filmspringserver/controller/AvatarController.java
@@ -8,21 +8,24 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/users/avatar")
+@RequestMapping("/api/avatars/")
 public class AvatarController {
 
     @Autowired
     AvatarService avatarService;
 
-    @GetMapping(value = "/{user_id}")
-    public ResponseEntity<Object> getAvatar(@PathVariable long user_id) {
-        return ResponseEntity.ok(avatarService.getAvatar(user_id));
+//    USERS
+
+    @GetMapping("/users/{user_id}")
+    public ResponseEntity<Object> getAvatarUser(@PathVariable long user_id) {
+        return ResponseEntity.ok(avatarService.getAvatarUser(user_id));
     }
 
-    @PutMapping("/{user_id}")
-    public ResponseEntity<Object> uploadAvatar(@RequestParam("file") MultipartFile file, @PathVariable long user_id) {
+    @PutMapping("/users/{user_id}")
+    public ResponseEntity<Object> uploadAvatarUser(@RequestParam("file") MultipartFile file,
+                                               @PathVariable long user_id) {
         try {
-            avatarService.storeAvatar(user_id, file);
+            avatarService.storeAvatarUser(user_id, file);
             return ResponseEntity.status(HttpStatus.OK).body("Success");
 
         } catch (Exception e) {
@@ -30,9 +33,34 @@ public class AvatarController {
         }
     }
 
-    @DeleteMapping("/{user_id}")
-    public ResponseEntity<Object> deleteAvatar(@PathVariable long user_id) {
-        avatarService.deleteAvatar(user_id);
+    @DeleteMapping("/users/{user_id}")
+    public ResponseEntity<Object> deleteAvatarUser(@PathVariable long user_id) {
+        avatarService.deleteAvatarUser(user_id);
+        return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
+    }
+
+//    GROUPS
+
+    @GetMapping("/groups/{group_id}")
+    public ResponseEntity<Object> getAvatarGroup(@PathVariable long group_id) {
+        return ResponseEntity.ok(avatarService.getAvatarGroup(group_id));
+    }
+
+    @PutMapping("/groups/{group_id}")
+    public ResponseEntity<Object> uploadAvatarGroup(@RequestParam("file") MultipartFile file,
+                                               @PathVariable long group_id) {
+        try {
+            avatarService.storeAvatarGroup(group_id, file);
+            return ResponseEntity.status(HttpStatus.OK).body("Success");
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(e);
+        }
+    }
+
+    @DeleteMapping("/groups/{group_id}")
+    public ResponseEntity<Object> deleteAvatarGroup(@PathVariable long group_id) {
+        avatarService.deleteAvatarGroup(group_id);
         return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
     }
 

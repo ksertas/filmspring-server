ba030ea804662dbb6fb790d72eac6d49d6549cf6
ksertas <k.serkanertas@gmail.com>
2021-12-17 00:47:08 +0100

    add email regex for more accurate validation

--- src/main/java/nl/serkanertas/filmspringserver/dto/NewUserPostRequestDto.java
+++ src/main/java/nl/serkanertas/filmspringserver/dto/NewUserPostRequestDto.java
@@ -7,16 +7,14 @@ import javax.validation.constraints.Size;
 public class NewUserPostRequestDto {
 
     @NotBlank
-    @Size(min = 3, max = 32)
+    @Size(min = 3, max = 32, message = "must be between 3 and 32 characters long.")
     private String username;
 
-    @NotBlank
-    @Email
-    private String email;
 
+    // Email regex from: http://emailregex.com/
     @NotBlank
-    @Size(min = 8, max = 32)
-    private String password;
+    @Email(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    private String email;
 
     public String getUsername() {
         return username;
@@ -34,11 +32,4 @@ public class NewUserPostRequestDto {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }

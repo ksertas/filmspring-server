8a4dcba3beaf165ae9f982a601beb50af51cf84a
ksertas <k.serkanertas@gmail.com>
2021-12-17 01:21:13 +0100

    map incoming DTO to entity

--- src/main/java/nl/serkanertas/filmspringserver/service/UserService.java
+++ src/main/java/nl/serkanertas/filmspringserver/service/UserService.java
@@ -1,15 +1,37 @@
 package nl.serkanertas.filmspringserver.service;
 
+import nl.serkanertas.filmspringserver.dto.NewUserPostRequestDto;
 import nl.serkanertas.filmspringserver.model.User;
 import nl.serkanertas.filmspringserver.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class UserService  {
 
     @Autowired
     private UserRepository userRepository;
+
+    private AvatarService avatarService;
+
+    public UserService(@Lazy AvatarService avatarService) {
+        this.avatarService = avatarService;
+    }
+
+
+    public String createUser(NewUserPostRequestDto newUserPostRequestDto) throws IOException {
+        User user = new User();
+        user.setUsername(newUserPostRequestDto.getUsername());
+        user.setEmail(newUserPostRequestDto.getEmail());
+        user.setEnabled(false);
+        user.setAvatarUser(avatarService.setDefaultAvatarUser());
+        userRepository.save(user);
+
+        return user.getUsername();
+    }
 
     public User getUser(long user_id) {
         return userRepository.findById(user_id).get();

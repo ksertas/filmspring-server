3dcac045119afd43896ead184d1fc8c0dea0c724
ksertas <k.serkanertas@gmail.com>
2021-12-20 21:58:19 +0100

    add film search containing query

--- src/main/java/nl/serkanertas/filmspringserver/repository/FilmRepository.java
+++ src/main/java/nl/serkanertas/filmspringserver/repository/FilmRepository.java
@@ -2,7 +2,10 @@ package nl.serkanertas.filmspringserver.repository;
 
 import nl.serkanertas.filmspringserver.model.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FilmRepository extends JpaRepository<Film, Long> {}
+public interface FilmRepository extends JpaRepository<Film, Long> {
+    Iterable<Film> findFIlmByTitleContainsIgnoreCase(@Param("s") String query);
+}

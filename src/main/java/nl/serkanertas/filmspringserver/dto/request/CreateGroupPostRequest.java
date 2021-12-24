48164551d9688e3a70c2b33bd7dd40d88d3c8d70
ksertas <k.serkanertas@gmail.com>
2021-12-24 16:53:07 +0100

    add group name regex validation

--- src/main/java/nl/serkanertas/filmspringserver/dto/request/CreateGroupPostRequest.java
+++ src/main/java/nl/serkanertas/filmspringserver/dto/request/CreateGroupPostRequest.java
@@ -1,11 +1,15 @@
 package nl.serkanertas.filmspringserver.dto.request;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class CreateGroupPostRequest {
+
+    // Groupname regex from https://stackoverflow.com/a/12019115
     @NotBlank
     @Size(min = 4, max = 32)
+    @Pattern(regexp = "^(?=.{3,32}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$")
     private String groupName;
 
     public String getGroupName() {
